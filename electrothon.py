b75a154af179a88a4303e94356be184a4ad74aa2
manyalalhall <manyasinghlalhall@gmail.com>
2025-03-09 00:07:57 +0530

    Fixed Render deployment and Tesseract setup

--- electrothon.py
+++ electrothon.py
@@ -8,7 +8,9 @@ import re
 import numpy as np
 from PIL import Image
 import easyocr
+import os 
 
+pytesseract.pytesseract.tesseract_cmd = os.path.join(os.getcwd(), "tesseract", "tesseract")
 app = FastAPI()
 @app.get("/")
 def read_root():
